58cb30a7cf00ef33b7109d0272eed3dd8a19fc44
Turgut Turkmen <turguturkmen@gmail.com>
2023-10-15 21:22:58 +0000

    eslint configs are updated.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.listen(3000, () => console.log("App is running on port 3000"))
+app.listen(3000, () => console.log('App is running on port 3000'))
